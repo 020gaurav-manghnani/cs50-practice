ce6a5e440efe9cfa4a9fae7ffc4733bd3b8f2547
gaurav manghnani <gauravmanghnani2@gmail.com>
2022-06-10 09:05:14 +0530

    BST_insertion

--- BST2.js
+++ BST2.js
@@ -1,45 +1,50 @@
 class Node{
-    constructor(value){
-        this.value=value;
-        this.right=null;
+    constructor(value) {
+        this.value=null;
         this.left=null;
+        this.right=null;
+    }
+}
+class BST{
+    constructor(){
+        this.root=null;
+    }
+    insert(value){
+        var newNode= new Node(value);
+        if(this.root===null){
+            this.root=newNode;
+            return this;
+        }
+        else{
+            var current=this.root;
+            while(true){
+                if(value<current.value){
+                    if(current.left===null){
+                        current.left=newNode;
+                        return this;
+                    }
+                    else{
+                        current=current.left;
+                    }
+                }
+                else if(value>current.value){
+                    if(current.right===null){
+                        current.right=newNode
+                        return this;
+                    }
+                    else{
+                        current=current.right;
+                    }
+
+                }
+            }
+        }
+        
     }
 }
 
- class BST{
-     constructor(){
-         this.root=null;
-     }
-     insert(value){
-         var newNode=new Node(value);
-         if(!this.root){
-             this.root=value;
-         }else{
-              var current=this.root;
-              while(true){
-                  if(value < current.value){
-                      if(current.left===null){
-                      current.left=newNode;
-                      }
-                      else{
-                          current=current.left;
-                      }
-                  }else if(value > current.value){
-                      if(current.right===null){
-                          current.right=newNode;
-                      }else{
-                          current=current.right;
-                      }
-                      
-                  }
-                  
-                  
-              }
-         }
-     }
-
- }
-
- var tree= new Node();
- tree.insert(10);
- console.log(tree);
+var tree=new BST()
+tree.insert(10);
+tree.insert(9);
+tree.insert(11);
+console.log(tree);
