1f8147bd0d65fe48d8db38835bed12659a1b5f06
gaurav manghnani <gauravmanghnani2@gmail.com>
2022-06-10 12:01:50 +0530

    bfs()

--- BST2.js
+++ BST2.js
@@ -1,6 +1,6 @@
 class Node{
-    constructor(value) {
-        this.value=null;
+    constructor(value){
+        this.value=value;
         this.left=null;
         this.right=null;
     }
@@ -10,41 +10,19 @@ class BST{
         this.root=null;
     }
     insert(value){
-        var newNode= new Node(value);
+        var newNode=new Node(value);
         if(this.root===null){
             this.root=newNode;
-            return this;
+            console.log(this.root);
         }
         else{
-            var current=this.root;
-            while(true){
-                if(value<current.value){
-                    if(current.left===null){
-                        current.left=newNode;
-                        return this;
-                    }
-                    else{
-                        current=current.left;
-                    }
-                }
-                else if(value>current.value){
-                    if(current.right===null){
-                        current.right=newNode
-                        return this;
-                    }
-                    else{
-                        current=current.right;
-                    }
+            var current=this.root
+            console.log(current);
 
-                }
-            }
         }
-        
     }
 }
 
 var tree=new BST()
-tree.insert(10);
-tree.insert(9);
-tree.insert(11);
-console.log(tree);
+tree.insert(10)
+// console.log(tree)
